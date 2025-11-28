c1830ffddbda7f6a698ab89472dbdd01b355c413
Soumya Prakash Biswas <110045139+soumyaPbiswas@users.noreply.github.com>
2025-11-28 15:51:50 +0530

    Fix formatting of Flask app initialization

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from app.routes import main
 
-app = Flask(__name__, template_folder='templates')  # 👈 explicitly specify the template folder
+app = Flask(__name__, template_folder='templates')  
 app.register_blueprint(main)
 
 if __name__ == '__main__':
